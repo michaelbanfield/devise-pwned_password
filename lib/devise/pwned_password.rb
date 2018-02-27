d9f20ed7ff97841afa3c60c8ff3627582b7e7d71
Damon Morgan <damon.morgan@gmail.com>
2018-02-27 20:54:37 +0000

    Fail silently for Net::HTTP exceptions
    Some of the most common exceptions Net::HTTP
    requests raise are Timeout::Error, Errno::ECONNRESET
    and Net::HTTPBadResponse. Those are now ignored and
    do not prevent signing up.
    
    The default Net::HTTP timeout setting for open
    and read is 60 seconds, which is too long to block
    a sign up for, so more reasonable defaults are set
    with the option to override them.

--- lib/devise/pwned_password.rb
+++ lib/devise/pwned_password.rb
@@ -4,8 +4,10 @@ require "devise"
 require "devise/pwned_password/model"
 
 module Devise
-  mattr_accessor :min_password_matches
+  mattr_accessor :min_password_matches, :pwned_password_open_timeout, :pwned_password_read_timeout
   @@min_password_matches = 1
+  @@pwned_password_open_timeout = 5
+  @@pwned_password_read_timeout = 5
 
   module PwnedPassword
   end
