752bcfb01761d2c2083d93719f79cf20a17e0da9
Tyler Rick <tyler@tylerrick.com>
2020-04-24 17:25:17 -0700

    Add missing tests for updating user and User.min_password_matches_warn
    Organize tests a bit

--- test/devise/pwned_password_test.rb
+++ test/devise/pwned_password_test.rb
@@ -3,42 +3,63 @@
 require "test_helper"
 
 class Devise::PwnedPassword::Test < ActiveSupport::TestCase
-  test "should deny validation for a pwned password" do
-    password = "password"
-    user = User.create email: "example@example.org", password: password, password_confirmation: password
-    assert_not user.valid?, "User with pwned password shoud not be valid."
-    assert_match /\Ahas appeared in a data breach \d{7,} times\z/, user.errors[:password].first
+  def setup
+    User.min_password_matches = 1
+    User.min_password_matches_warn = nil
   end
 
-  test "should deny validation for a pwned password: {count: 1}" do
-    password = "password"
-    user = User.create email: "example@example.org", password: password, password_confirmation: password
-    pwned_password = Minitest::Mock.new
-    pwned_password.expect :pwned_count, 1
-    Pwned::Password.stub :new, pwned_password do
-      assert_not user.valid?, "User with pwned password shoud not be valid."
+  class WhenPwned < Devise::PwnedPassword::Test
+    test "should deny validation and set pwned_count" do
+      user = pwned_password_user
+      assert_not user.valid?
+      assert_match /\Ahas appeared in a data breach \d{7,} times\z/, user.errors[:password].first
+      assert user.pwned_count > 0
     end
-    assert_match "has appeared in a data breach", user.errors[:password].first
-    pwned_password.verify
+
+    test "when pwned_count < min_password_matches, is considered valid" do
+      user = pwned_password_user
+      User.min_password_matches = 999_999_999
+      assert user.valid?
+      assert user.pwned_count > 0
+    end
   end
 
-  test "should accept validation for a password not in the dataset" do
-    # This test will be unavoidably flaky
-    password = "fddkasnsdddghjt"
-    user = User.create email: "example@example.org", password: password, password_confirmation: password
-    assert user.valid?, "User with password not in the dataset should be valid."
+  class WhenNotPwned < Devise::PwnedPassword::Test
+    test "should accept validation and set pwned_count" do
+      user = valid_password_user
+      assert user.valid?
+      assert_equal user.pwned_count, 0
+    end
+
+    test "when password changed to a pwned password: should add error if pwned_count > min_password_matches_warn || pwned_count > min_password_matches" do
+      user = valid_password_user
+
+      # *not* pwned_count > min_password_matches_warn
+      password = "password"
+      user.update password: password, password_confirmation: password
+      User.min_password_matches_warn = 999_999_999
+      assert user.valid?
+      assert_not user.pwned_count > User.min_password_matches_warn
+
+      # pwned_count > min_password_matches_warn
+      User.min_password_matches_warn = 1
+      User.min_password_matches      = 999_999_999
+      assert_not user.valid?
+      assert user.pwned_count > User.min_password_matches_warn
+    end
   end
 
-  test "should return a non-zero count for a pwned password" do
+  def pwned_password_user
     password = "password"
     user = User.create email: "example@example.org", password: password, password_confirmation: password
-    assert_not_equal user.pwned_count, 0, "User with pwned password should not have a zero count."
+    assert user.errors.size > 0
+    user
   end
 
-  test "should return a count of zero for a password not in the dataset" do
-    # This test will be unavoidably flaky
+  def valid_password_user
     password = "fddkasnsdddghjt"
     user = User.create email: "example@example.org", password: password, password_confirmation: password
-    assert_equal user.pwned_count, 0, "User with password not in the dataset should have a zero count."
+    assert_equal 0, user.errors.size
+    user
   end
 end
